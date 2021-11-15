2d9bc3739743b32f0d562519a7d1d121d42de0a8
ssuarezs <31489965+ssuarezs@users.noreply.github.com>
2021-11-15 17:59:31 +0000

    Deploying to gh-pages from @ TPI-Desarrollo/nextghp@d365f6f9be60e6b7b3ce42da5e094cf9959f033c 🚀

---
+++ zones/data.js
@@ -0,0 +1,33 @@
+import { prefix } from '../../utils/prefix.js';
+
+const datazones = [
+	{
+		name: 'CAFETERIA',
+		desc: 'Conformacion de Equipos',
+		img: `${prefix}/imgs/zones/cafe.png`,
+		link: 'https://ingenieria.unal.edu.co/tpi/index.php/24propuestas',
+	},
+	{
+		name: 'REUNIONES',
+		desc: 'Webinars para todos',
+		img: `${prefix}/imgs/zones/reun.png`,
+		link: '/zones/asesor',
+	},
+	{
+		name: 'LOBBY',
+		desc: 'Tus tareas inician aqui',
+		img: `${prefix}/imgs/zones/lobby.png`,
+		link: '/zones/asesor',
+		type: 'twover'
+	},
+	{
+		name: 'ASESORES',
+		desc: 'Resuelve tus dudas',
+		img: `${prefix}/imgs/zones/asesor.png`,
+		link: '/zones/asesor',
+		type: 'twohor'
+	},
+]
+
+export const getDataZones = () => datazones
+
