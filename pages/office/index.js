4a550a27a60a02ebb37e93e95073d551d18d70f2
ssuarezs <suarez.santiago.113@gmail.com>
2021-11-19 20:09:27 -0500

    fix office imgs

--- pages/office/index.js
+++ pages/office/index.js
@@ -20,13 +20,13 @@ const office = [
 		name: 'OFICINAS',
 		desc: '2-4 pm',
 		link: 'https://docs.google.com/spreadsheets/d/1nLoG42SGWYe4xi7BHpKh6goHHLkImDSj7XKUu0XjFU0/edit#gid=1528342584',
-		img: `${prefix}/imgs/office/btn.png`
+		img: '/imgs/office/btn.png'
 	},
 	{
 		name: 'OFICINAS',
 		desc: '4-6 pm',
 		link: 'https://docs.google.com/spreadsheets/d/1nLoG42SGWYe4xi7BHpKh6goHHLkImDSj7XKUu0XjFU0/edit#gid=23923283',
-		img: `${prefix}/imgs/office/btn.png`
+		img: '/imgs/office/btn.png'
 	}
 ]
 
